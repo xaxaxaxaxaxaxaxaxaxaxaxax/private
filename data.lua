3d35a597d6e28122924e8e2b6723dfc6be1cda73
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-07-30 18:52:01 -0400

    Update licenses

--- data.lua
+++ data.lua
@@ -1,3 +1,4 @@
 return {
-    ['N1GW-2E8V-5Z4A-9HTV'] = {user_id = 1149023627069620297, hwid = "None", username = 'xaxaxaxaxaxaxaxaxxaxaxaxaxaxax'}
+    ['N1GW-2E8V-5Z4A-9HTV'] = {user_id = 1149023627069620297, hwid = "None", username = 'xaxaxaxaxaxaxaxaxxaxaxaxaxaxax'},
+    ['BPO8-PVXO-2YB7-JD8R'] = {user_id = 1231791380603605087, hwid = "None", username = 'kovingalld_'}
 }
