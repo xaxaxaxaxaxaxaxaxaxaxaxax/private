1eeaad03a445b7c3f36bbf3df5f377fbcd310f43
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-07-30 19:13:19 -0400

    Update licenses

--- data.lua
+++ data.lua
@@ -1,4 +1,4 @@
 return {
-    ['N1GW-2E8V-5Z4A-9HTV'] = {user_id = 1149023627069620297, hwid = "None", username = 'xaxaxaxaxaxaxaxaxxaxaxaxaxaxax'},
+    ['N1GW-2E8V-5Z4A-9HTV'] = {user_id = 1149023627069620297, hwid = "174f7a74-a701-4251-b1f3-7006e8f6f6a6", username = 'xaxaxaxaxaxaxaxaxxaxaxaxaxaxax'},
     ['BPO8-PVXO-2YB7-JD8R'] = {user_id = 1231791380603605087, hwid = "None", username = 'kovingalld_'}
 }
