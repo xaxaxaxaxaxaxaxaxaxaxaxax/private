8a34e1989932a355880cd35c5f6253e4c72ec06c
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-07-31 14:25:30 -0400

    Update licenses

--- data.lua
+++ data.lua
@@ -4,5 +4,5 @@ return {
     ['U76H-QZLU-FYZ2-GSQP'] = {user_id = 1075942500679811124, hwid = "891EC84C-CA7E-42E1-A4F2-9F5719EA4ED5", username = 'xkylsgrave.'},
     ['SNP0-GTCL-EBMI-DG63'] = {user_id = 1111369658109546729, hwid = "833C7E12-E1DE-4584-B487-5B27034B04A3", username = 'midleg'},
     ['AH7S-F1HB-8PAG-K8RS'] = {user_id = 1249380503820177528, hwid = "56a92570-8ec0-40a3-92f5-240e92d4ff90", username = 'wraithsins_'},
-    ['TFOK-K14S-0M3E-4A31'] = {user_id = 1160356736507596851, hwid = "2F82DD90-D96D-4567-8B18-986C97D87EF3", username = 'invinsion'}
+    ['TFOK-K14S-0M3E-4A31'] = {user_id = 1160356736507596851, hwid = "None", username = 'invinsion'}
 }
