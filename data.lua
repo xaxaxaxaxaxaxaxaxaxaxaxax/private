f0b089335bd85c446b153fa8c7e897cc5a9e93f1
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-08-02 10:20:57 -0400

    Update licenses

--- data.lua
+++ data.lua
@@ -6,7 +6,7 @@ return {
     ['66E1-19V5-CHOR-12KS'] = {user_id = 1030765647162003486, hwid = "None", username = 'dx6s'},
     ['NW5K-EDTU-XKMI-FSMO'] = {user_id = 1253777463490777178, hwid = "CAA1EC90-0E55-44DB-8423-3A3532770B7A", username = 'floxk13'},
     ['V1I8-5AMR-9HA6-ZY7R'] = {user_id = 1243955280732094515, hwid = "None", username = 'invinsionn'},
-    ['MH1O-XG1K-N5NX-1PTI'] = {user_id = 1045756792036151317, hwid = "None", username = 'qyriqev'},
+    ['MH1O-XG1K-N5NX-1PTI'] = {user_id = 1045756792036151317, hwid = "6EC51FBD-FCA1-4D5F-AA50-91C8D2915020", username = 'qyriqev'},
     ['H8DA-JEZ0-CSO6-MWV9'] = {user_id = 1129611096815304774, hwid = "None", username = 'fartboy979'},
     ['XH4N-SWRH-APY0-13NT'] = {user_id = 1178717471474192457, hwid = "None", username = 'backindh'},
     ['7YOE-AR97-QKBX-D5SZ'] = {user_id = 1214980417669697602, hwid = "None", username = 'bankai09680'},
