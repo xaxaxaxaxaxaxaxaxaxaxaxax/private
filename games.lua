326912447380f810bebf072c204b11155fcacc90
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-08-07 09:27:21 -0400

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,23 @@
+if gameId == 2788229376 then
+    MainEvent, Argument = "MainEvent", "UpdateMousePosI"
+elseif gameId == 9825515356 then
+    MainEvent, Argument = "MainEvent", "MousePosUpdate"
+elseif gameId == 5602055394 then
+    MainEvent, Argument = "MAINEVENT", "MousePos"
+elseif gameId == 17403265390 or gameId == 17403166075 or gameId == 17403262882 or gameId == 15763494605 then
+    MainEvent, Argument = "MAINEVENT", "MOUSE"
+elseif gameId == 15186202290 then
+    MainEvent, Argument = "MAINEVENT", "MOUSE"
+elseif gameId == 11143225577 then
+    MainEvent, Argument = "MAINEVENT", "UpdateMousePos"
+elseif gameId == 15166543806 then
+    MainEvent, Argument = "MAINEVENT", "MoonUpdateMousePos"
+elseif gameId == 17319408836 then
+    MainEvent, Argument = "MAINEVENT", "UpdateMousePos"
+elseif gameId == 16033173781 or gameId == 7213786345 then
+    MainEvent, Argument = "MainEvent", "UpdateMousePosI"
+elseif gameId == 18110728826 then
+    MainEvent, Argument = "MainEvent", "UpdateMousePos"
+else
+    MainEvent, Argument = "MainEvent", "UpdateMousePos"
+end
