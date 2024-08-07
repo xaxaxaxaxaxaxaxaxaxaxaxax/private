f52028c5a71b7bfa5c945e4d2b0289b922d346f7
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-08-07 09:29:58 -0400

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,23 +1,16 @@
-if gameId == 2788229376 then
-    MainEvent, Argument = "MainEvent", "UpdateMousePosI"
-elseif gameId == 9825515356 then
-    MainEvent, Argument = "MainEvent", "MousePosUpdate"
-elseif gameId == 5602055394 then
-    MainEvent, Argument = "MAINEVENT", "MousePos"
-elseif gameId == 17403265390 or gameId == 17403166075 or gameId == 17403262882 or gameId == 15763494605 then
-    MainEvent, Argument = "MAINEVENT", "MOUSE"
-elseif gameId == 15186202290 then
-    MainEvent, Argument = "MAINEVENT", "MOUSE"
-elseif gameId == 11143225577 then
-    MainEvent, Argument = "MAINEVENT", "UpdateMousePos"
-elseif gameId == 15166543806 then
-    MainEvent, Argument = "MAINEVENT", "MoonUpdateMousePos"
-elseif gameId == 17319408836 then
-    MainEvent, Argument = "MAINEVENT", "UpdateMousePos"
-elseif gameId == 16033173781 or gameId == 7213786345 then
-    MainEvent, Argument = "MainEvent", "UpdateMousePosI"
-elseif gameId == 18110728826 then
-    MainEvent, Argument = "MainEvent", "UpdateMousePos"
-else
-    MainEvent, Argument = "MainEvent", "UpdateMousePos"
-end
+return {
+    [2788229376] = {MainEvent = "MainEvent", Argument = "UpdateMousePosI"},
+    [9825515356] = {MainEvent = "MainEvent", Argument = "MousePosUpdate"},
+    [5602055394] = {MainEvent = "MAINEVENT", Argument = "MousePos"},
+    [17403265390] = {MainEvent = "MAINEVENT", Argument = "MOUSE"},
+    [17403166075] = {MainEvent = "MAINEVENT", Argument = "MOUSE"},
+    [17403262882] = {MainEvent = "MAINEVENT", Argument = "MOUSE"},
+    [15763494605] = {MainEvent = "MAINEVENT", Argument = "MOUSE"},
+    [15186202290] = {MainEvent = "MAINEVENT", Argument = "MOUSE"},
+    [11143225577] = {MainEvent = "MAINEVENT", Argument = "UpdateMousePos"},
+    [15166543806] = {MainEvent = "MAINEVENT", Argument = "MoonUpdateMousePos"},
+    [17319408836] = {MainEvent = "MAINEVENT", Argument = "UpdateMousePos"},
+    [16033173781] = {MainEvent = "MainEvent", Argument = "UpdateMousePosI"},
+    [7213786345] = {MainEvent = "MainEvent", Argument = "UpdateMousePosI"},
+    [18110728826] = {MainEvent = "MainEvent", Argument = "UpdateMousePos"}
+}
