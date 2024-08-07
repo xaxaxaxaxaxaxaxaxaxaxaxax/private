3e3bab0cdbcd7fefa80bf17ee2fbbb2d8c192fe1
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-08-07 17:14:50 -0400

    Update nanoware.lua

--- nanoware.lua
+++ nanoware.lua
@@ -1,58 +1,69 @@
-getgenv().nano = {
-    ['Main'] = {
-        ['License'] = "KEY HERE",
-        ['Type'] = "1",
-        ['Notifications'] = { 
-            ['Enabled'] = true,
-            ['Duration'] = 2.5
+getgenv().configuration = {
+    ["Main"] = {
+        ["Keybind"] = Enum.KeyCode.Q,
+        ["Prediction"] = 0.15634,
+        ["Point"] = {
+            ["Ground"] = "HumanoidRootPart",
+            ["Air"] = "UpperTorso"
         },
+        ["Notifications"] = true,
+        ["License"] = "UW4H-R64U-CLTW-8H1V",
+        ["Type"] = "1" -- Type 1 is Camlock, Type 2 is Target Aim.
     },
-    ['Aimbot'] = {
-        ['Keybind'] = Enum.KeyCode.C,
-        ['Prediction'] = 0.15634,
-        ['Smoothness'] = {
-            ['Enabled'] = false,
-            ['Amount'] = 0.11
-        },
-        ['Hit'] = {
-            ['Part'] = "HumanoidRootPart",
-            ['Air'] = {
-                ['Enabled'] = true,
-                ['Jump'] = "LowerTorso",
-                ['Fall'] = "UpperTorso"
-            }
-        },
-        ['Highlight Target'] = {
-            ['Enabled'] = true,
-            ['Fill Color'] = Color3.new(1, 1, 1),
-            ['Outline Color'] = Color3.new(0, 0, 0),
-            ['Transparency'] = 0.5,
-        },
-        ['Offsets'] = {
-            ['Jump'] = {
-                ['Enabled'] = true,
-                ['Amount'] = -1
-            }
-        },
+    ["Aimbot"] = {
+        ["Smoothness"] = {
+            ["Enabled"] = false,
+            ["Amount"] = 0.5
+        }
     },
-    ['Checks'] = {
-        ['Death'] = true,
-        ['Grabbed'] = true
-    },
-    ['HvH'] = {
-        ['Cframe'] = {
-            ['Enabled'] = true,
-            ['Amount'] = 3,
-            ['Keybind'] = Enum.KeyCode.V,
+    ["Crosshair"] = {
+        ["Enabled"] = true,
+        ["Width"] = 1.5,
+        ["Length"] = 10,
+        ["Radius"] = 11,
+        ["Color"] = Color3.new(1, 1, 1),
+        ["Outline Color"] = Color3.new(0, 0, 0),
+        ["Spin"] = {
+            ["Enabled"] = true,
+            ["Speed"] = 150,
+            ["Max"] = 340,
+            ["Style"] = Enum.EasingStyle.Sine
         },
-        ['Strafe'] = { 
-            ['Enabled'] = true,
-            ['Distance'] = 10,
-            ['Height'] = 1,
-            ['Speed'] = 32,
-            ['Keybind'] = Enum.KeyCode.N,
-        },
+        ["Resize"] = {
+            ["Enabled"] = true,
+            ["Speed"] = 150,
+            ["Min"] = 5,
+            ["Max"] = 22
+        }
     },
+    ["Box"] = {
+        ["Enabled"] = true,
+        ["Size"] = Vector3.new(4, 5, 3),
+        ["Color"] = Color3.new(1, 1, 1),
+        ["Material"] = Enum.Material.ForceField,
+        ["Transparency"] = 0.8
+    },
+    ["Highlight"] = {
+        ["Enabled"] = true,
+        ["Fill Color"] = Color3.new(1, 1, 1),
+        ["Outline Color"] = Color3.new(0, 0, 0),
+        ["Fill Transparency"] = 0.5,
+        ["Outline Transparency"] = 0
+    },
+    ["Strafe"] = {
+        ["Enabled"] = true,
+        ["Keybind"] = Enum.KeyCode.X,
+        ["Speed"] = 32,
+        ["Distance"] = 8,
+        ["Height"] = 14
+    },
+    ["Cframe Walk"] = {
+        ["Enabled"] = true,
+        ["Keybind"] = Enum.KeyCode.V,
+        ["Amount"] = 2
+    },
+    ["Checks"] = {
+        ["Death"] = true,
+        ["Grabbed"] = true
+    }
 }
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxaxaxaxax/private/main/main.lua"))()
