349baa393eb60ac35e1bec5d9f39b24967dbe76f
aladinjr <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-08-08 23:52:38 -0400

    Update licenses

--- data.lua
+++ data.lua
@@ -2,7 +2,7 @@ return {
     ['BPO8-PVXO-2YB7-JD8R'] = {user_id = 1231791380603605087, hwid = "AC21A7FD-9C88-496D-8721-A15D317DBCD7", username = 'kovingalld_'},
     ['SNP0-GTCL-EBMI-DG63'] = {user_id = 1111369658109546729, hwid = "833C7E12-E1DE-4584-B487-5B27034B04A3", username = 'midleg'},
     ['AH7S-F1HB-8PAG-K8RS'] = {user_id = 1249380503820177528, hwid = "56a92570-8ec0-40a3-92f5-240e92d4ff90", username = 'wraithsins_'},
-    ['2AQY-ZJ5W-WPCL-SRDX'] = {user_id = 926320233868820521, hwid = "d674109b-f3fa-4ce6-afd2-ceaf7b95c05b", username = 'kryptonite1112'},
+    ['2AQY-ZJ5W-WPCL-SRDX'] = {user_id = 926320233868820521, hwid = "None", username = 'kryptonite1112'},
     ['66E1-19V5-CHOR-12KS'] = {user_id = 1030765647162003486, hwid = "None", username = 'dx6s'},
     ['NW5K-EDTU-XKMI-FSMO'] = {user_id = 1253777463490777178, hwid = "CAA1EC90-0E55-44DB-8423-3A3532770B7A", username = 'floxk13'},
     ['V1I8-5AMR-9HA6-ZY7R'] = {user_id = 1243955280732094515, hwid = "None", username = 'invinsionn'},
